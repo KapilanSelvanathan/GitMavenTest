9f0057979e48f1c983db91c3353bce4f415866db
kapilan <kapilan.ks18@gmail.com>
2016-08-26 21:33:35 +0100

    Initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp(){
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		driver=new ChromeDriver();
+			
+	}
+	@Test
+	public void doLogin() {
+		driver.get("https://www.facebook.com/");
+		
+		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("kapilan@yahoo.com");
+		
+		driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("ffff");
+		
+		driver.findElement(By.xpath(".//*[@id='loginbutton']")).click();
+		
+		System.out.println("test");
+			
+	}
+	@AfterSuite
+	public void tearDown()
+	{
+		driver.quit();		
+	}
+}
